e7583fdbed94f4d8b23c8d69c9862a427735d9c8
Axel Demborg <axel.demborg@gmail.com>
2024-01-03 17:10:15 +0100

    Random paletts

--- src/lib/color.ts
+++ src/lib/color.ts
@@ -14,3 +14,24 @@ export const defaultPalette: Palette = {
 	detail1: '#adb8d0',
 	detail2: '#96a0c0'
 };
+
+function lerp(a:number, b:number, t: number) {
+    return a + t * (b - a);
+}
+
+export function makePalette(): Palette {
+    const hue = Math.random() * 360;
+    const lightest = 50 + Math.random() * 50;
+    const darkest = Math.random() * (100 - lightest);
+    const saturation = 50  + Math.random() * 50;
+
+    const leafHue = Math.random() > 0.5 ? hue : hue + 180
+
+    return {
+        petal: `hsl(${hue}deg, ${saturation}%, ${lightest}%)`,
+        detail1: `hsl(${hue}deg, ${saturation}%, ${lerp(lightest, darkest, 0.25)}%)`,
+        detail2: `hsl(${hue}deg, ${saturation}%, ${lerp(lightest, darkest, 0.5)}%)`,
+        background: `hsl(${hue}deg, ${saturation}%, ${lerp(lightest, darkest, 0.75)}%)`,
+        leaf: `hsl(${leafHue}deg, ${saturation}%, ${darkest}%)`
+    }
+}
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
 	import Pattern from '$lib/Pattern.svelte';
+	import { makePalette } from '$lib/color';
 </script>
 
-<Pattern />
+<Pattern palette={makePalette()}/>
