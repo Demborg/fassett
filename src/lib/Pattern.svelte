e3e67ed649ebc563c4172ed5a25fc43c5f11ff70
Axel Demborg <axel.demborg@gmail.com>
2024-01-03 12:33:32 +0100

    Draw circles with respectful distance

--- src/lib/Pattern.svelte
+++ src/lib/Pattern.svelte
@@ -1,14 +1,52 @@
 <script lang="ts">
 	import { onMount } from 'svelte';
 	import { defaultPalette, type Palette } from './color';
+	const nFlowers = 200;
 	const size = 1000;
+	const minDistance = 200;
 	export let seed: number = 42;
 	export let palette: Palette = defaultPalette;
 	let canvas: HTMLCanvasElement;
 
+	type Point = { x: number; y: number };
+
 	$: draw(seed, palette);
 
 	onMount(() => draw(seed, palette));
+
+	function tilingCircle(ctx: CanvasRenderingContext2D, x: number, y: number, r: number) {
+		let offsets = [-size, 0, size];
+		offsets.forEach((dx) => {
+			offsets.forEach((dy) => {
+				ctx.moveTo(x + dx, y + dy);
+				ctx.arc(x + dx, y + dy, r, 0, 2 * Math.PI);
+				console.log('Circle at', x, y);
+			});
+		});
+	}
+
+	function distance(a: Point, b: Point): number {
+		return Math.sqrt(Math.pow(a.x - b.x, 2) + Math.pow(a.y - b.y, 2));
+	}
+
+	function minTilingDistance(a: Point, b: Point): number {
+		let offsets = [-size, 0, size];
+		return Math.min(
+			...offsets.map((dx) =>
+				Math.min(...offsets.map((dy) => distance(a, { x: b.x + dx, y: b.y + dy })))
+			)
+		);
+	}
+
+	function filterMinDistance(positions: Point[], minDistance: number): Point[] {
+		return positions.reduce(
+			(filtered: Point[], incomming: Point) =>
+				filtered.some((old: Point) => minTilingDistance(old, incomming) < minDistance)
+					? filtered
+					: [...filtered, incomming],
+			[]
+		);
+	}
 
 	function draw(seed: number, palette: Palette) {
 		if (!canvas) {
@@ -21,6 +59,17 @@
 
 		ctx.fillStyle = palette.background;
 		ctx.fillRect(0, 0, size, size);
+
+		const candidates = [...Array(nFlowers)].map(() => {
+			return { x: Math.random() * size, y: Math.random() * size };
+		});
+		const positions = filterMinDistance(candidates, minDistance);
+
+		ctx.fillStyle = palette.leaf;
+		positions.forEach((p) => {
+			tilingCircle(ctx, p.x, p.y, 100);
+		});
+		ctx.fill();
 	}
 </script>
 
