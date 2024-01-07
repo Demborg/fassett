6855db8f502104d0641a12bea7f97be9d730220e
Axel Demborg <axel.demborg@gmail.com>
2024-01-07 12:25:42 +0100

    Add route for eassy embedding

---
+++ src/routes/embed/+page.svelte
@@ -0,0 +1,23 @@
+<script lang="ts">
+	import Pattern from '$lib/Pattern.svelte';
+	import { type Palette, makePalette } from '$lib/color';
+	let palette: Palette = makePalette();
+</script>
+
+<button on:click={() => (palette = makePalette())}>
+	<Pattern {palette} />
+</button>
+
+<style>
+	button {
+		border: none;
+		margin: 0;
+		padding: 0;
+		background-color: transparent;
+	}
+
+	:global(body) {
+		margin: 0;
+		padding: 0;
+	}
+</style>
